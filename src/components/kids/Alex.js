0999ae78db57385b329eebbb7de890a6f57273c7
Alex <SweetTea2324@gmail.com>
2018-10-13 13:56:24 -0600

    Alex's page

---
+++ src/components/kids/Alex.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+
+const styles = theme => ({
+  root: {
+    textAlign: 'center',
+    padding: 50,
+    backgroundColor: 'blue',
+  },
+  textColor: {
+    color: 'silver'
+  },
+});
+
+function Alex(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <Typography variant="display2" gutterBottom className={classes.textColor}>Hello, this is Alex!</Typography>
+      <img src="/assets/shoes_repo/png/059-shoe-1.png"/>
+    </div>
+  );
+}
+
+Alex.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Alex);
