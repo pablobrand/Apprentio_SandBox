c5e53bbe6b12f0fde778be79ac5a49e568294b97
Armando <mandosteel.06@gmail.com>
2018-10-27 13:41:49 -0600

    Update src/components/kids/Armando.js
    addons

--- src/components/kids/Armando.js
+++ src/components/kids/Armando.js
@@ -2,16 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
   root: {
-    textAlign: 'center',
-    padding: 50,
-    backgroundColor: 'black',
+    flexGrow: 1
   },
   textColor: {
-    color: 'white',
+    color: 'grey',
+  },
+  paperOne: {
+    padding: theme.spacing.unit * 2,
+    color: theme.palette.text.secondary,
+    background: 'grey',
+    color: 'black',
+    textAlign: "center",
   },
 });
 
@@ -21,6 +27,27 @@ function Armando(props) {
     <div className={classes.root}>
       <Typography variant="display2" gutterBottom className={classes.textColor}>Hello, this is Armando!</Typography>
       <img src = "/assets/shoes_repo/png/058-soccer-shoe.png"/>
+      <Grid item xs={12}>
+          <Paper className={classes.paperOne}>Hello World</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paperOne}>How is your day going?</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paperOne}>Good</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paperOne}>Bad</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paperOne}>Okay</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paperOne}>Alright</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paperOne}>No Comment</Paper>
+        </Grid>
     </div>
   );
 }
