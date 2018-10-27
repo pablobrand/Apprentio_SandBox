5630507ab8b7b4a7db446a3830bc65a310a96a40
Argenis <argeniscasillas80@gmail.com>
2018-10-27 13:54:59 -0600

    Update src/components/kids/Argenis.js

--- src/components/kids/Argenis.js
+++ src/components/kids/Argenis.js
@@ -7,23 +7,20 @@ import Typography from '@material-ui/core/Typography';
 const styles = theme => ({
   root: {
     textAlign: 'center',
-    padding: 50
-  },
-  root: {
-    textAlign: 'center',
-    padding:100
+    padding: 50,
+    backgroundColor: 'green',
   },
   textColor: {
-    color: 'red'
+    color: 'green'
   },
 });
-
 function Argenis(props) {
   const { classes } = props;
   return (
     <div className={classes.root}>
-      <Typography variant="display2" gutterBottom>Hello, this is Argenis!</Typography>
-      <img src = "/assets/shoes_repo/png/059-shoe-1.png" />
+      <Typography variant="display2" gutterBottom>Whats diddly doin, this is Argenis!</Typography>
+      <img src = "/assets/shoes_repo/png/055-sneaker.png" />
+      <img scr = "/assets/apprentio_logo.png" />     
     </div>
   );
 }
